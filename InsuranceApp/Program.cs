0bb3c4853f091cee77065b35abf824f4c1f89641
Dylan Sheardown <dylashea@stjohns.school.nz>
2025-03-21 12:05:29 +1300

    Add project files.

---
+++ InsuranceApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+
+namespace InsuranceApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Ascii Art
+            string asciiArt = @"
+
+    ██ ███    ██ ███████ ██    ██ ██████   █████  ███    ██  ██████ ███████      █████  ██████  ██████  
+    ██ ████   ██ ██      ██    ██ ██   ██ ██   ██ ████   ██ ██      ██          ██   ██ ██   ██ ██   ██ 
+    ██ ██ ██  ██ ███████ ██    ██ ██████  ███████ ██ ██  ██ ██      █████       ███████ ██████  ██████  
+    ██ ██  ██ ██      ██ ██    ██ ██   ██ ██   ██ ██  ██ ██ ██      ██          ██   ██ ██      ██      
+    ██ ██   ████ ███████  ██████  ██   ██ ██   ██ ██   ████  ██████ ███████     ██   ██ ██      ██      
+                                                                                                        
+                                                                                                        
+     ";
+            //Add different colors to the text
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(asciiArt);
+            Console.ResetColor();
+            // Underline the text
+            Console.WriteLine("Welcome to the Insurance App!");
+
+
+        }
+    }
+}
+
+
